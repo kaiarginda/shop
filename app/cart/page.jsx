aaf548e8eab05cf5715b3c9da610926a7a84b3c1
tornike <tornike.tanda@gmail.com>
2023-11-27 10:04:51 -0800

    3sadl;

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,12 +1,60 @@
+// // // import React from "react";
+// // // import { cookies } from "next/headers";
+// // // import { verify } from "jsonwebtoken";
+// // // import AddToCart from "../components/AddToCart";
+// // // const page = () => {
+// // //   const cookieStore = cookies();
+// // //   const token = cookieStore.get("OutSideJWT");
+
+// // //   if (!token) {
+// // //     return (
+// // //       <div className="text-center">
+// // //         <h1 className="text-3xl font-bold text-red-500 p-4">
+// // //           You Are Not Logged In{" "}
+// // //         </h1>
+// // //         <p className="text-blue-500">
+// // //           <a href="/login">Log In</a>
+// // //         </p>
+// // //       </div>
+// // //     );
+// // //   } else {
+// // //     const user = verify(token.value, "secret");
+// // //     return (
+// // //       <div>
+// // //         <AddToCart user={user} />
+// // //       </div>
+// // //     );
+// // //   }
+// // // };
+
+// // // export default page;
+
 // // import React from "react";
 // // import { cookies } from "next/headers";
 // // import { verify } from "jsonwebtoken";
 // // import AddToCart from "../components/AddToCart";
-// // const page = () => {
+
+// // const checkUserLoggedIn = () => {
 // //   const cookieStore = cookies();
 // //   const token = cookieStore.get("OutSideJWT");
 
 // //   if (!token) {
+// //     return {
+// //       loggedIn: false,
+// //     };
+// //   } else {
+// //     const user = verify(token.value, "secret");
+// //     return {
+// //       loggedIn: true,
+// //       user,
+// //     };
+// //   }
+// // };
+
+// // const page = () => {
+// //   const { loggedIn, user } = checkUserLoggedIn();
+
+// //   if (!loggedIn) {
 // //     return (
 // //       <div className="text-center">
 // //         <h1 className="text-3xl font-bold text-red-500 p-4">
@@ -18,7 +66,6 @@
 // //       </div>
 // //     );
 // //   } else {
-// //     const user = verify(token.value, "secret");
 // //     return (
 // //       <div>
 // //         <AddToCart user={user} />
@@ -34,24 +81,31 @@
 // import { verify } from "jsonwebtoken";
 // import AddToCart from "../components/AddToCart";
 
-// const checkUserLoggedIn = () => {
-//   const cookieStore = cookies();
-//   const token = cookieStore.get("OutSideJWT");
+// let hasCheckedUser = false;
+// let loggedIn = false;
+// let user = null;
 
-//   if (!token) {
-//     return {
-//       loggedIn: false,
-//     };
-//   } else {
-//     const user = verify(token.value, "secret");
-//     return {
-//       loggedIn: true,
-//       user,
-//     };
+// const checkUserLoggedIn = () => {
+//   if (!hasCheckedUser) {
+//     const cookieStore = cookies();
+//     const token = cookieStore.get("OutSideJWT");
+
+//     if (token) {
+//       try {
+//         user = verify(token.value, "secret");
+//         loggedIn = true;
+//       } catch (error) {
+//         console.error("Error verifying token:", error);
+//       }
+//     }
+
+//     hasCheckedUser = true;
 //   }
+
+//   return { loggedIn, user };
 // };
 
-// const page = () => {
+// const Page = () => {
 //   const { loggedIn, user } = checkUserLoggedIn();
 
 //   if (!loggedIn) {
@@ -74,58 +128,12 @@
 //   }
 // };
 
-// export default page;
+// export default Page;
 
 import React from "react";
-import { cookies } from "next/headers";
-import { verify } from "jsonwebtoken";
-import AddToCart from "../components/AddToCart";
 
-let hasCheckedUser = false;
-let loggedIn = false;
-let user = null;
-
-const checkUserLoggedIn = () => {
-  if (!hasCheckedUser) {
-    const cookieStore = cookies();
-    const token = cookieStore.get("OutSideJWT");
-
-    if (token) {
-      try {
-        user = verify(token.value, "secret");
-        loggedIn = true;
-      } catch (error) {
-        console.error("Error verifying token:", error);
-      }
-    }
-
-    hasCheckedUser = true;
-  }
-
-  return { loggedIn, user };
+const page = () => {
+  return <div>Hello World</div>;
 };
 
-const Page = () => {
-  const { loggedIn, user } = checkUserLoggedIn();
-
-  if (!loggedIn) {
-    return (
-      <div className="text-center">
-        <h1 className="text-3xl font-bold text-red-500 p-4">
-          You Are Not Logged In{" "}
-        </h1>
-        <p className="text-blue-500">
-          <a href="/login">Log In</a>
-        </p>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <AddToCart user={user} />
-      </div>
-    );
-  }
-};
-
-export default Page;
+export default page;
