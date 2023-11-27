d7c24e398df3f7c84f7c0302dd523b141938e167
tornike <tornike.tanda@gmail.com>
2023-11-27 09:21:10 -0800

    34 comm3

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,35 +1,90 @@
-import React from "react";
+// import React from "react";
+// import CardComponent from "../components/CardComponent";
+// import { connectMongoDB } from "../mongodb";
+// import { cookies } from "next/headers";
+// import { verify } from "jsonwebtoken";
+// import AddToCart from "../components/AddToCart";
+// const page = async ({ searchParams }) => {
+//   await connectMongoDB();
+//   const cookieStore = cookies();
+//   const token = cookieStore.get("OutSideJWT");
+
+//   if (!token) {
+//     return (
+//       <div className="text-center">
+//         <h1 className="text-3xl font-bold text-red-500 p-4">
+//           You Are Not Logged In{" "}
+//         </h1>
+//         <p className="text-blue-500">
+//           <a href="/login">Log In</a>
+//         </p>
+//         {/* <AddToCart /> */}
+//       </div>
+//     );
+//   } else {
+//     const user = verify(token.value, "secret");
+//     return (
+//       <div>
+//         <AddToCart user={user} />
+//         {/* <CardComponent user={user} /> */}
+//       </div>
+//     );
+//   }
+// };
+
+// export default page;
+
+import React, { useEffect, useState } from "react";
 import CardComponent from "../components/CardComponent";
 import { connectMongoDB } from "../mongodb";
 import { cookies } from "next/headers";
 import { verify } from "jsonwebtoken";
 import AddToCart from "../components/AddToCart";
-const page = async ({ searchParams }) => {
-  await connectMongoDB();
-  const cookieStore = cookies();
-  const token = cookieStore.get("OutSideJWT");
 
-  if (!token) {
-    return (
-      <div className="text-center">
-        <h1 className="text-3xl font-bold text-red-500 p-4">
-          You Are Not Logged In{" "}
-        </h1>
-        <p className="text-blue-500">
-          <a href="/login">Log In</a>
-        </p>
-        {/* <AddToCart /> */}
-      </div>
-    );
-  } else {
-    const user = verify(token.value, "secret");
-    return (
-      <div>
+const Page = ({ searchParams }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await connectMongoDB();
+        const cookieStore = cookies();
+        const token = cookieStore.get("OutSideJWT");
+
+        if (!token) {
+          setLoggedIn(false);
+        } else {
+          const decodedUser = verify(token.value, "secret");
+          setUser(decodedUser);
+          setLoggedIn(true);
+        }
+      } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+      }
+    };
+
+    fetchData();
+  }, []); // The empty dependency array ensures that this effect runs only once on mount
+
+  return (
+    <div>
+      {loggedIn ? (
         <AddToCart user={user} />
-        {/* <CardComponent user={user} /> */}
-      </div>
-    );
-  }
+      ) : (
+        // Uncomment the line below if you want to render CardComponent as well
+        // <CardComponent user={user} />
+        <div className="text-center">
+          <h1 className="text-3xl font-bold text-red-500 p-4">
+            You Are Not Logged In{" "}
+          </h1>
+          <p className="text-blue-500">
+            <a href="/login">Log In</a>
+          </p>
+        </div>
+      )}
+    </div>
+  );
 };
 
-export default page;
+export default Page;
