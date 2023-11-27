20c1ae36e64ee7779d6b0f4d1fcdddffa1ab758f
tornike <tornike.tanda@gmail.com>
2023-11-27 10:40:49 -0800

    3sdsa;

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -81,25 +81,28 @@ import { cookies } from "next/headers";
 import { verify } from "jsonwebtoken";
 import AddToCart from "../components/AddToCart";
 const page = () => {
-  return (
-    <div>
-      <AddToCart user={{}} />
-    </div>
-  );
+  const cookieStore = cookies();
+  const token = cookieStore.get("OutSideJWT");
 
-  // if (!token) {
-  //   return (
-  //     <div className="text-center">
-  //       <h1 className="text-3xl font-bold text-red-500 p-4">
-  //         You Are Not Logged In{" "}
-  //       </h1>
-  //       <p className="text-blue-500">
-  //         <a href="/login">Log In</a>
-  //       </p>
-  //     </div>
-  //   );
-  // } else {
-  // }
+  if (!token) {
+    return (
+      <div className="text-center">
+        <h1 className="text-3xl font-bold text-red-500 p-4">
+          You Are Not Logged In{" "}
+        </h1>
+        <p className="text-blue-500">
+          <a href="/login">Log In</a>
+        </p>
+      </div>
+    );
+  } else {
+    const user = verify(token.value, "secret");
+    return (
+      <div>
+        <AddToCart user={{ user }} />
+      </div>
+    );
+  }
 };
 
 export default page;
